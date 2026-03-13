af2e9e704d8c2bd46b9818993eb97474954f7697
AaronRuizzz <aaron.r.m@um.es>
2026-03-13 09:53:47 +0100

    CODE - Inclusión de audios provisionales

--- src/hooks/usePirateAudio.js
+++ src/hooks/usePirateAudio.js
@@ -1,9 +1,9 @@
 import { useEffect, useMemo } from "react";
 
 const DEFAULT_SOUNDS = {
-  click: "https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8a73467.mp3?filename=click-124467.mp3",
-  error: "https://cdn.pixabay.com/download/audio/2022/03/10/audio_8f0f8d1f6f.mp3?filename=error-126627.mp3",
-  success: "https://cdn.pixabay.com/download/audio/2022/03/15/audio_5f60365aa0.mp3?filename=success-1-6297.mp3",
+  click: "/audio/click.wav",
+  error: "/audio/error.wav",
+  success: "/audio/success.wav",
 };
 
 function safePlay(audioInstance) {
